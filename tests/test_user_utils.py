aabcb319aa1bef629fe169a2d97fda8c914b6280
AlmazKasimov <crimsonri@mail.ru>
2026-02-02 15:55:45 +0300

    tests

--- tests/test_user_utils.py
+++ tests/test_user_utils.py
@@ -1,11 +1,13 @@
 # tests/test_user_utils.py
+import pytest
 from user_utils import check_age
 
-def test_check_age_under_18():
-    assert check_age(17) == "Вы ещё несовершеннолетний"
-
-def test_check_age_18():
-    assert check_age(18) == "Вам больше 18 лет"
-
-def test_check_age_over_18():
-    assert check_age(25) == "Вам больше 18 лет"
+@pytest.mark.parametrize("age, expected", [
+    (17, "Вы ещё несовершеннолетний"),
+    (18, "Вам больше 18 лет"),
+    (25, "Вам больше 18 лет"),
+    (0, "Вы ещё несовершеннолетний"),
+    (100, "Вам больше 18 лет"),
+])
+def test_check_age(age, expected):
+    assert check_age(age) == expected
