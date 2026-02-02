09d102ad9a098574cf662bbdfa6dcfcc8710c675
AlmazKasimov <crimsonri@mail.ru>
2026-02-02 16:47:48 +0300

    tests

--- tests/test_demo_ui.py
+++ tests/test_demo_ui.py
@@ -2,21 +2,15 @@
 from playwright.sync_api import sync_playwright
 
 
-def test_login_form():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+def test_login_form(page):
+   # Стабильный сайт
+    page.goto("https://the-internet.herokuapp.com/login")
 
-        # Стабильный сайт
-        page.goto("https://the-internet.herokuapp.com/login")
+    # Вводим данные
+    page.fill("#username", "tomsmith")
+    page.fill("#password", "SuperSecretPassword!")
+    page.click("button[type='submit']")
 
-        # Вводим данные
-        page.fill("#username", "tomsmith")
-        page.fill("#password", "SuperSecretPassword!")
-        page.click("button[type='submit']")
-
-        # Проверяем успешный вход
-        assert page.is_visible(".flash.success")
-        assert "You logged into a secure area!" in page.text_content(".flash")
-
-        browser.close()
+    # Проверяем успешный вход
+    assert page.is_visible(".flash.success")
+    assert "You logged into a secure area!" in page.text_content(".flash")
